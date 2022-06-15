e2d7691edf3c23cbcb1bf03824628552e88c563d
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-15 10:46:00 -0700

    First and Second

---
+++ Java-1A.java
@@ -0,0 +1,9 @@
+
+
+public class Java-1A
+{
+	public static void main(String args[])
+	{
+		System.out.printl("Hello Java Lover")
+	}
+}
---
+++ Java1/Java1.java
@@ -0,0 +1,7 @@
+public class Java1 {
+  public static void main(String[] args) {
+    System.out.println("I love Coding");
+    System.out.println("That's why I am trying to be a Software Engineeer");
+    System.out.println("Oopz");
+  }
+}
