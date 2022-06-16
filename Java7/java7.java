cc6481c5bbf97c8ab48f3c10e037841239ca4119
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-16 10:11:10 -0700

    Math Operators

---
+++ Java7/java7.java
@@ -0,0 +1,21 @@
+public class java7
+{
+	public static void main(String arg[])
+	{
+		int no1, no2, ans;
+		no1 = 20;
+		no2 = 3;
+		
+		ans = no1 + no2;
+		System.out.println(ans);
+		
+		ans = no1 - no2;
+		System.out.println(ans);
+		
+		ans = no1 / no2;
+		System.out.println(ans);
+		
+		ans = no1 % no2;
+		System.out.println(ans);
+	}
+}
