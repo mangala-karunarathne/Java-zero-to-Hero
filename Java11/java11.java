7a6699ee06f19db49b984a64bf786011460023a0
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-17 12:36:35 -0700

    Logical Operators

---
+++ Java11/java11.java
@@ -0,0 +1,11 @@
+public class java11 {
+    public static void main(String arg[]) {
+        int maths = 75;
+        int science = 70;
+        if (maths > 60 && science > 55) {
+            System.out.println(" Pass ");
+        } else {
+            System.out.println(" Fail ");
+        }
+    }
+}
