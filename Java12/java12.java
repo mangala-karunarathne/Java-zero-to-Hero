ff90698ae882c6827507809fa0ed02c8b8236454
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-17 13:03:20 -0700

    Logical Operators 2

---
+++ Java12/java12.java
@@ -0,0 +1,11 @@
+public class java12 {
+    public static void main(String arg[]) {
+        int maths = 25;
+        int science = 70;
+        if (maths > 60 || science > 55) { // or statement eighther one is enough to proceed
+            System.out.println(" Pass ");
+        } else {
+            System.out.println(" Fail ");
+        }
+    }
+}
