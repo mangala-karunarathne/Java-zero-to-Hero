ca8df1d1250084062d4adeaa75b153befc813332
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 11:37:50 -0700

    While

---
+++ Practice/src/main/java/com/mycompany/practice/azwhile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.practicepro;
+
+/**
+ *
+ * @author Win
+ */
+public class azwhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        int num = 10;
+        while(num<15){
+            
+            System.out.println(num + "  Mangala");
+            
+            num++;
+        }
+    }
+}
