66f1c9b43bb16a922b5b5ddd713612151a1353a1
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 11:36:52 -0700

    Increment 2

---
+++ Practice/src/main/java/com/mycompany/practice/increment2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.practicepro;
+
+/**
+ *
+ * @author Win
+ */
+public class increment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+        int madhu = 10;
+        
+        while(madhu<30){
+            
+            System.out.println(madhu + " It's a bit of unlucky Madhu...");
+            
+            madhu += 5;
+        }
+    }
+}
