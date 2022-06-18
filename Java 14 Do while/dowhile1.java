6a715b3c62d7fd3cd29ce61f64508af3855149a0
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 15:49:53 -0700

    Do While

---
+++ Java 14 Do while/dowhile1.java
@@ -0,0 +1,12 @@
+public class dowhile1 {
+    public static void main(String args[]) {
+
+        int smk = 12;
+        do {
+
+            System.out.println(smk);
+            smk++;
+        } while (smk < 15);
+
+    }
+}
