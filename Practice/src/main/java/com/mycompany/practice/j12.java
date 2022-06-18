4a55a074f3d9b0a3901f3b9a57620807c6ecd1f6
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-17 23:22:35 -0700

    Switch

---
+++ Practice/src/main/java/com/mycompany/practice/j12.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.practice;
+
+/**
+ *
+ * @author Win
+ */
+public class j12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int age = 17;
+        
+        switch (age){
+            
+            case 15 :
+                System.out.println("Your age is 15");
+                break;
+                
+            case 16 :
+                System.out.println("Your age is 16");
+                break;
+                
+            case 17 :
+                System.out.println("Your age is 17");
+                break;
+        }
+    }
+}
