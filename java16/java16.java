75a2077f935b82c927b0723f051d1bfbcc1825fa
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 20:51:05 -0700

    Use Methods

---
+++ java16/java16.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.practicepro;
+
+/**
+ *
+ * @author Win
+ */
+public class java16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+        firstsmk();
+        java16 mangalaObj = new java16();
+       mangalaObj.secondsmk();
+                
+    }
+    public static void firstsmk() {
+        
+        System.out.println("First Math Try");
+        
+    }
+    public void secondsmk() {
+        
+        System.out.println("Second Math Try");
+        
+    }
+}
