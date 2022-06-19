7d776fb9846408ff2ddb72e53350dad1d2afa50a
mangala-karunarathne <mangalaicc@gmail.com>
2022-06-18 20:48:32 -0700

    for loop

---
+++ java15/java15.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.practicepro;
+
+/**
+ *
+ * @author Win
+ */
+public class java15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        for(int smk = 10; smk<20; smk +=3)
+        {
+            System.out.println(smk + " Mangala");
+        }
+    }
+}
