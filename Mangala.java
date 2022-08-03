c199cd6f4755dc98a4d55724ee79e6eb50d2c788
mangala-karunarathne <mangalaicc@gmail.com>
2022-08-03 04:58:08 -0700

    Star Pattern 1

---
+++ Mangala.java
@@ -0,0 +1,23 @@
+// Print a pattern like right angle triangle
+
+import java.util.Scanner;
+public class Mangala {
+
+  public static void main(String[] args)
+
+			{
+   	          int i,j,n;
+
+				System.out.print("Input number of n : ");
+            	Scanner input = new Scanner(System.in);
+				n = input.nextInt();
+  
+					for(i=1;i<=n;i++)
+					   {
+						   for(j=1;j<=i;j++)
+							System.out.print(i);
+							System.out.println("");
+					  }
+            }
+            } 
+			
